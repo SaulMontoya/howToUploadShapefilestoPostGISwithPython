889fd179b5fd000db963448dd4b8674ac4816799
Saul Montoya <saulmontoya@gidahatari.com>
2021-02-24 22:11:56 +0000

    second commit

---
+++ runSetup.sh
@@ -0,0 +1,10 @@
+sudo bash -c "echo listen_addresses = \'*\' >> /etc/postgresql/12/main/postgresql.conf"
+sudo bash -c 'echo host all all 0.0.0.0/0 md5 >> /etc/postgresql/12/main/pg_hba.conf'
+sudo bash -c 'echo host all all ::/0 md5 >> /etc/postgresql/12/main/pg_hba.conf'
+sudo service postgresql start
+sudo service postgresql status
+sudo -u postgres psql -c "CREATE USER gis WITH PASSWORD 'gis';"
+sudo -u postgres psql -c "CREATE DATABASE geodatabase OWNER gis;"
+sudo -u postgres psql -c "\c geodatabase;"
+sudo -u postgres psql -c "CREATE EXTENSION postgis;"
+
